fd81931b4953a403f703cddf1d9e4a6a8ecaa629
Skibidle <griffinmortensen145@gmail.com>
2025-02-10 14:17:26 -0600

    Create search.js

---
+++ static/scripts/modules/search.js
@@ -0,0 +1,7 @@
+export function setupSearch() {
+    const searchInput = document.getElementById('searchInput');
+    searchInput.addEventListener('input', (e) => {
+        const query = e.target.value;
+        // Implement search logic here
+    });
+}
