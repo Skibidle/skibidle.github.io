5565bdc72a49158bfad3280429ce6920bceb34d7
Skibidle <griffinmortensen145@gmail.com>
2025-02-10 14:16:57 -0600

    Update componentloader.js

--- static/scripts/modules/componentloader.js
+++ static/scripts/modules/componentloader.js
@@ -1,37 +1,13 @@
 export class ComponentLoader {
-    constructor() {
-        this.components = {
-            header: '/components/header.html',
-            cards: '/components/cards.html',
-            footer: '/components/footer.html'
-        };
-    }
-
-    async loadComponent(component) {
-        const response = await fetch(component);
-        if (!response.ok) throw new Error(`Failed to load ${component}`);
-        return await response.text();
-    }
-
     async loadComponents() {
-        try {
-            const loadTasks = Object.entries(this.components).map(
-                async ([name, path]) => {
-                    const html = await this.loadComponent(path);
-                    document.getElementById(`${name}-container`).innerHTML = html;
-                }
-            );
-            
-            await Promise.all(loadTasks);
-            this.injectArticles();
-        } catch (error) {
-            throw new Error(`Component loading error: ${error.message}`);
-        }
+        const components = ['navbar', 'footer'];
+        const loadPromises = components.map(name => this.loadComponent(name));
+        await Promise.all(loadPromises);
     }
 
-    injectArticles() {
-        // Article data would be imported from /data/articles.js
-        const articleGrid = document.getElementById('card-container');
-        // Dynamic article injection logic here
+    async loadComponent(name) {
+        const response = await fetch(`/components/${name}.html`);
+        const html = await response.text();
+        document.getElementById(`${name}-container`).innerHTML = html;
     }
 }
