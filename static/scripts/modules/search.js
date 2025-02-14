754614b8a6790dfafe74a6d381f371b1e92851b7
Skibidle <griffinmortensen145@gmail.com>
2025-02-13 22:18:58 -0600

    Delete static directory

--- static/scripts/modules/search.js
+++
@@ -1,7 +0,0 @@
-export function setupSearch() {
-    const searchInput = document.getElementById('searchInput');
-    searchInput.addEventListener('input', (e) => {
-        const query = e.target.value;
-        // Implement search logic here
-    });
-}
